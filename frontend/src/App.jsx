02320832d96b0f53194e047d21cbe95b251334cd
Nxnonon <suphakanphadee@gmail.com>
2025-07-08 12:35:04 +0700

    delete test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Test from "./test";
-import Home from "./Page/Home";
 import { HomeIcon } from "lucide-react";
 import Appdistance from "./root/AppRoot";
 
--- frontend/src/Page/Toto.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Toto = () => {
-  return <div className="text-center text-9xl text-red-700 ">toto</div>;
-};
-
-export default Toto;
